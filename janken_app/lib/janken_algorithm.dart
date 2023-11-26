18d6ff624b147b98df70f255a13a4da5ecd1bd3a
azuma <bassetchocolat79@gmail.com>
2023-11-26 23:57:15 +0900

    create janken-app

---
+++ janken_app/lib/janken_algorithm.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+// [返り値の型] [関数の名前]( [引数の型] [引数の名前] ) {
+//   [関数の中で実行する命令];
+// }
+
+class JankenGame {
+  String play(int userChoice) {
+    List<String> jankenHand = ["グー", "チョキ", "パー"];
+    int computerChoice = Random().nextInt(3);
+
+    String result;
+    if (userChoice == computerChoice) {
+      result = "あいこ";
+    } else if ((userChoice == 0 && computerChoice == 1) ||
+        (userChoice == 2 && computerChoice == 0)) {
+      result = "あなたの勝ち";
+    } else {
+      result = "あなたの負け";
+    }
+    return "あなたの手: ${jankenHand[userChoice]}, \n コンピュータの手: ${jankenHand[computerChoice]}, \n 結果: $result";
+  }
+}
--- janken_app/lib/main.dart
+++ janken_app/lib/main.dart
@@ -1,41 +1,93 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'janken_algorithm.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'じゃんけんアプリ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: JankenPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
+//StatefulWidget：Stateオブジェクトを通じて状態を管理
+class JankenPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _JankenPageState createState() => _JankenPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _JankenPageState extends State<JankenPage> {
+  String message = "じゃんけん...";
+  // final：一度final とつけた変数に値を入れたなら、再代入はできない
+  final JankenGame game = JankenGame();
+  bool isGameActive = true; // ゲームがアクティブかどうかを追跡
+
+  void playGame(int userChoice) {
+    if (isGameActive) {
+      // ゲームがアクティブな場合のみプレイ可能
+      setState(() {
+        message = game.play(userChoice);
+        isGameActive = false; // ゲームの状態を非アクティブに更新
+      });
+
+      Timer(Duration(seconds: 3), () {
+        setState(() {
+          message = "じゃんけん...";
+          isGameActive = true; // ゲームの状態を再びアクティブに更新
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('じゃんけんアプリ'),
       ),
-      body: const Center(
-        child: Text(
-          'Basic Flutter 2023',
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              message,
+              style: TextStyle(fontSize: 24),
+            ),
+            Row(
+              //MainAxisAlignment:余白均等
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //ElevatedButtonボタンの種類
+                ElevatedButton(
+                  // =>：引数を取らずに_playGame()メソッドを呼び出す短い関数
+                  onPressed: isGameActive
+                      ? () => playGame(0)
+                      : null, // ゲーム状態に基づくボタンの有効化/無効化
+                  child: Text('グー'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  // 条件 ? 真の場合の式 : 偽の場合の式
+                  onPressed: isGameActive ? () => playGame(1) : null,
+                  child: Text('チョキ'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: isGameActive ? () => playGame(2) : null,
+                  child: Text('パー'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
