d3cdcd96fdd66dac85e7afd510b8c21c2ee68a80
azuma <bassetchocolat79@gmail.com>
2024-02-10 11:14:42 +0900

    UI修正、intl ver変更

--- qiita_app/lib/widgets/article_container.dart
+++ qiita_app/lib/widgets/article_container.dart
@@ -4,77 +4,87 @@ import 'package:qiita_app/models/article.dart';
 
 class ArticleContainer extends StatelessWidget {
   const ArticleContainer({
-    super.key,
+    Key? key,
     required this.article,
-  });
+  }) : super(key: key);
 
   final Article article;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          // color: AppColors.white,
-          ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 8,
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(article.userIconUrl),
-              radius: 19,
-            ),
-            const SizedBox(width: 8),
-            Expanded(
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(article.userIconUrl),
+                radius: 19,
+              ),
+              const SizedBox(width: 8),
+              Expanded(
                 child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  article.title,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.black,
-                  ),
-                ),
-                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      '＠${article.userName}',
+                      article.title,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                       style: const TextStyle(
-                        fontSize: 12,
-                        color: AppColors.secondary,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.black,
                       ),
                     ),
-                    const SizedBox(width: 4),
-                    Text(
-                      '投稿日:${article.postedDate}',
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: AppColors.secondary,
-                      ),
-                    ),
-                    const SizedBox(width: 4),
-                    Text(
-                      'いいね：${article.likesCount}',
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: AppColors.secondary,
-                      ),
+                    Row(
+                      children: [
+                        Text(
+                          '＠${article.userName}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: AppColors.secondary,
+                          ),
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          '投稿日:${article.postedDate}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: AppColors.secondary,
+                          ),
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          'いいね：${article.likesCount}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: AppColors.secondary,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            )),
+              ),
+            ],
+          ),
+        ),
+        const Row(
+          children: [
+            SizedBox(
+                width:
+                    16 + 19 * 2 + 8), // CircleAvatarの直径と左のPadding、そして間のスペースを加算
+            Expanded(
+              child: Divider(
+                thickness: 0.5,
+                color: AppColors.secondary,
+              ),
+            ),
           ],
         ),
-      ),
+      ],
     );
   }
 }
